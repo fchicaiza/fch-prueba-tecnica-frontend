b01ba5f32fb0ae759898c7886defbb1013b0d8d4
fchicaiza <fernando_chicaiza_g@hotmail.com>
2023-09-07 13:32:55 -0500

    Configure new Ionic Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import Page from './pages/Page';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -30,14 +29,8 @@ const App: React.FC = () => {
     <IonApp>
       <IonReactRouter>
         <IonSplitPane contentId="main">
-          <Menu />
           <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/folder/Inbox" />
-            </Route>
-            <Route path="/folder/:name" exact={true}>
-              <Page />
-            </Route>
+          
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
